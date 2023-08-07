d71fd04b1f663d41cefb768ccb862f6df10c8381
Andreea <andelenasamur@gmail.com>
2023-08-07 11:14:39 +0200

    countries routing module and lazyload

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
 
 
 
@@ -9,13 +13,18 @@ import { AboutPageComponent } from './pages/about-page/about-page.component';
   declarations: [
     AboutPageComponent,
     HomePageComponent,
+    SidebarComponent,
+    ContactPageComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [
     AboutPageComponent,
     HomePageComponent,
+    SidebarComponent,
+    ContactPageComponent
   ]
 })
 export class SharedModule { }
